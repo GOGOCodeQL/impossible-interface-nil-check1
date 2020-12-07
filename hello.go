fb1b0f767267516c5ef9f288c12baa0323e5872b
재영 정 <jjy600901@snu.ac.kr>
2020-12-07 17:06:35 +0900

    First Commit

---
+++ hello.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+//MyError is My Error :)
+type MyError struct {
+	Op   string
+	Path string
+	Err  error
+}
+
+func (E MyError) Error() string {
+	return E.Op + "bye!!!"
+}
+
+func returnsError() *MyError {
+	var p *MyError = nil
+	return p // Will always return a non-nil error.
+}
+
+func main() {
+	var e error = returnsError()
+	if e != nil {
+		println("Error!!")
+	} else {
+		fmt.Println("404 No Error") //unreachable
+	}
+}
